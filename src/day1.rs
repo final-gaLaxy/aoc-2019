b7774066f76d4575b0fbbc801ea6a7e34d5d7d63
final-gaLaxy <sebastian@genus.net.au>
2024-04-13 00:02:13 +1000

    complete part 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,13 @@
 use std::fmt::Write;
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &str) -> String {
-    let mut result: String = String::new();
-    write!(&mut result, "{}", input).unwrap();
-
-    result
+pub fn solve_part1(input: &str) -> u32 {
+    input.lines()
+         .map(|tok| {
+            let module = tok.trim().parse::<u32>().unwrap();
+            module / 3 - 2
+         })
+         .sum()
 }
 
 #[aoc(day1, part2)]
@@ -20,13 +22,29 @@ pub fn solve_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
+    // Part 1
     #[test]
     fn example1() {
-        assert_eq!(solve_part1("Hello"), "Hello")
+        assert_eq!(solve_part1("12"), 2)
     }
 
     #[test]
     fn example2() {
-        assert_eq!(solve_part2("World"), "World")
+        assert_eq!(solve_part1("14"), 2)
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(solve_part1("1969"), 654)
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(solve_part1("100756"), 33583)
+    }
+
+    #[test]
+    fn example5() {
+        assert_eq!(solve_part1("12\n14\n1969\n100756"), [2,2,654,33583].iter().sum())
     }
 }
