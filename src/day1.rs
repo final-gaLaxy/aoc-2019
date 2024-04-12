71e29b97261599bac6e11886e90da862d369078b
final-gaLaxy <sebastian@genus.net.au>
2024-04-13 00:14:24 +1000

    complete part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,21 +1,29 @@
-use std::fmt::Write;
-
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &str) -> u32 {
-    input.lines()
-         .map(|tok| {
-            let module = tok.trim().parse::<u32>().unwrap();
+pub fn solve_part1(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|tok| {
+            let module = tok.trim().parse::<i32>().unwrap();
             module / 3 - 2
-         })
-         .sum()
+        })
+        .sum()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &str) -> String {
-    let mut result: String = String::new();
-    write!(&mut result, "{}", input).unwrap();
-
-    result
+pub fn solve_part2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|tok| {
+            let mass = tok.trim().parse::<i32>().unwrap();
+            let mut fuel = mass / 3 - 2;
+            let mut sum = 0;
+            while fuel >= 0 {
+                sum += fuel;
+                fuel = fuel / 3 - 2;
+            }
+            sum
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -46,5 +54,31 @@ mod tests {
     #[test]
     fn example5() {
         assert_eq!(solve_part1("12\n14\n1969\n100756"), [2,2,654,33583].iter().sum())
+    }
+
+    // Part 2
+    #[test]
+    fn example6() {
+        assert_eq!(solve_part2("12"), 2)
+    }
+
+    #[test]
+    fn example7() {
+        assert_eq!(solve_part2("14"), 2)
+    }
+
+    #[test]
+    fn example8() {
+        assert_eq!(solve_part2("1969"), 966)
+    }
+
+    #[test]
+    fn example9() {
+        assert_eq!(solve_part2("100756"), 50346)
+    }
+
+    #[test]
+    fn example10() {
+        assert_eq!(solve_part2("12\n14\n1969\n100756"), [2,2,966,50346].iter().sum())
     }
 }
