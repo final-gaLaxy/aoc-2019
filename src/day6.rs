e8d82ee2e9cad9512cb0872245b0fd262e03e392
final-gaLaxy <sebastian@genus.net.au>
2024-04-24 20:39:50 +1000

    complete day 6 part 1

--- src/day6.rs
+++ src/day6.rs
@@ -1,9 +1,55 @@
-#[aoc(day1, part1)]
-pub fn solve_part1(input: &str) -> i32 {
-    0
+use std::collections::HashMap;
+
+#[aoc_generator(day6, part1)]
+pub fn input_generator(input: &str) -> HashMap<String, String> {
+    input
+        .trim()
+        .lines()
+        .map(|line|
+            line.split_at(line.find(')').unwrap())
+        )
+        .map(|(object, orbiter)| (&orbiter[1..], object))
+        .map(|(k, v)| (k.to_string(), v.to_string()))
+        .collect()
 }
 
-#[aoc(day1, part2)]
+pub fn solve(
+    name: &String,
+    satellites: &HashMap<String, String>,
+    orbits: &mut HashMap<String, i32>
+) -> i32 {
+    match orbits.get(name) {
+        Some(child) => *child,
+        None => {
+            if !satellites.contains_key(name) {
+                0
+            } else {
+                let value = 1 + solve(
+                    satellites.get(name).unwrap(),
+                        satellites,
+                        orbits
+                );
+                orbits.insert(name.to_string(), value);
+                *orbits.get(name).unwrap()
+            }
+        }
+    }
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(input: &HashMap<String, String>) -> i32 {
+    let satellites: HashMap<String, String> = input.clone();
+    let mut orbits: HashMap<String, i32> = HashMap::new();
+    let mut sum = 0;
+
+    for k in satellites.keys() {
+        sum += solve(k, &satellites, &mut orbits);
+    }
+
+    sum
+}
+
+#[aoc(day6, part2)]
 pub fn solve_part2(input: &str) -> i32 {
     0
 }
@@ -11,4 +57,13 @@ pub fn solve_part2(input: &str) -> i32 {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // Part 1
+    #[test]
+    fn example1() {
+        assert_eq!(
+            solve_part1(&input_generator("COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L")),
+            42
+        );
+    }
 }
