3d69ed2f8004fa4d08837bb573805a2150f392de
final-gaLaxy <sebastian@genus.net.au>
2024-04-25 17:36:01 +1000

    day 7: add structs for amplifiers

--- src/day7.rs
+++ src/day7.rs
@@ -1,6 +1,106 @@
 use std::{cmp, collections::VecDeque};
 use itertools::Itertools;
 
+struct Amplifier {
+    int_code: Vec<i32>,
+    halted: bool,
+}
+
+impl Amplifier {
+    fn new(int_code: &Vec<i32>) -> Amplifier {
+        Amplifier {
+            int_code: int_code.clone(),
+            halted: false
+        }
+    }
+
+    fn solve_with_inputs(&mut self, mut inputs: VecDeque<i32>) -> Vec<i32> {
+        let mut outputs: Vec<i32> = Vec::new();
+        let mut p = 0;
+        while p < self.int_code.len() {
+            let mut inst = self.int_code[p];
+
+            let op_code = inst % 100;
+            inst /= 100;
+
+            match op_code {
+                99 => break,
+                1 => { // Addition
+                    let p1: usize = if inst%10 == 1 { p+1 } else { self.int_code[p+1] as usize };
+                    inst /= 10;
+                    let p2: usize = if inst%10 == 1 { p+2 } else { self.int_code[p+2] as usize };
+                    let p3: usize = self.int_code[p+3] as usize;
+                    self.int_code[p3] = self.int_code[p1] + self.int_code[p2];
+                    p += 4;
+                },
+                2 => { // Multiplication
+                    let p1: usize = if inst%10 == 1 { p+1 } else { self.int_code[p+1] as usize };
+                    inst /= 10;
+                    let p2: usize = if inst%10 == 1 { p+2 } else { self.int_code[p+2] as usize };
+                    let p3: usize = self.int_code[p+3] as usize;
+                    self.int_code[p3] = self.int_code[p1] * self.int_code[p2];
+                    p += 4;
+                },
+                3 => { // Input
+                    let addr: usize = self.int_code[p+1] as usize;
+                    self.int_code[addr] = inputs.pop_front().unwrap();
+                    p += 2;
+                },
+                4 => { // Output
+                    let addr: usize = if inst%10 == 1 { p+1 } else { self.int_code[p+1] as usize };
+                    outputs.push(self.int_code[addr]);
+                    p += 2;
+                },
+                5 => { // jump-if-true
+                    let p1: usize = if inst%10 == 1 { p+1 } else { self.int_code[p+1] as usize };
+                    inst /= 10;
+                    let p2: usize = if inst%10 == 1 { p+2 } else { self.int_code[p+2] as usize };
+
+                    if self.int_code[p1] != 0 {
+                        p = self.int_code[p2] as usize;
+                    } else {
+                        p += 3;
+                    }
+                },
+                6 => { // jump-if-false
+                    let p1: usize = if inst%10 == 1 { p+1 } else { self.int_code[p+1] as usize };
+                    inst /= 10;
+                    let p2: usize = if inst%10 == 1 { p+2 } else { self.int_code[p+2] as usize };
+
+                    if self.int_code[p1] == 0 {
+                        p = self.int_code[p2] as usize;
+                    } else {
+                        p += 3;
+                    }
+                },
+                7 => { // less than
+                    let p1: usize = if inst%10 == 1 { p+1 } else { self.int_code[p+1] as usize };
+                    inst /= 10;
+                    let p2: usize = if inst%10 == 1 { p+2 } else { self.int_code[p+2] as usize };
+                    let p3: usize = self.int_code[p+3] as usize;
+
+                    self.int_code[p3] = if self.int_code[p1] < self.int_code[p2] { 1 } else { 0 };
+
+                    p += 4;
+                },
+                8 => { // equal to
+                    let p1: usize = if inst%10 == 1 { p+1 } else { self.int_code[p+1] as usize };
+                    inst /= 10;
+                    let p2: usize = if inst%10 == 1 { p+2 } else { self.int_code[p+2] as usize };
+                    let p3: usize = self.int_code[p+3] as usize;
+
+                    self.int_code[p3] = if self.int_code[p1] == self.int_code[p2] { 1 } else { 0 };
+
+                    p += 4;
+                }
+                _ => {}
+            }
+        }
+
+        outputs
+    }
+}
+
 #[aoc_generator(day7)]
 pub fn input_generator(input: &str) -> Vec<i32> {
     input
@@ -14,9 +114,17 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 pub fn solve_part1(int_code: &Vec<i32>) -> i32 {
     let mut max = 0;
     for perm in (0..=4).permutations(5) {
+        let amplifiers: &mut Vec<Amplifier> = &mut vec![];
+        for _ in 0..5 {
+            amplifiers.push(Amplifier::new(int_code));
+        }
+
         let mut output = 0;
-        for i in perm {
-            output = solve_with_inputs(int_code.clone(), VecDeque::from([i, output]))[0];
+        for (amp, input) in perm.iter().enumerate() {
+            output = amplifiers
+                .get_mut(amp)
+                .unwrap()
+                .solve_with_inputs(VecDeque::from([*input, output]))[0];
         }
         max = cmp::max(max, output);
     }
@@ -27,93 +135,6 @@ pub fn solve_part1(int_code: &Vec<i32>) -> i32 {
 #[aoc(day7, part2)]
 pub fn solve_part2(_input: &Vec<i32>) -> i32 {
     0
-}
-
-fn solve_with_inputs(p: Vec<i32>, mut inputs: VecDeque<i32>) -> Vec<i32> {
-    let mut program = p.clone();
-    let mut outputs: Vec<i32> = Vec::new();
-    let mut p = 0;
-    while p < program.len() {
-        let mut inst = program[p];
-
-        let op_code = inst % 100;
-        inst /= 100;
-
-        match op_code {
-            99 => break,
-            1 => { // Addition
-                let p1: usize = if inst%10 == 1 { p+1 } else { program[p+1] as usize };
-                inst /= 10;
-                let p2: usize = if inst%10 == 1 { p+2 } else { program[p+2] as usize };
-                let p3: usize = program[p+3] as usize;
-                program[p3] = program[p1] + program[p2];
-                p += 4;
-            },
-            2 => { // Multiplication
-                let p1: usize = if inst%10 == 1 { p+1 } else { program[p+1] as usize };
-                inst /= 10;
-                let p2: usize = if inst%10 == 1 { p+2 } else { program[p+2] as usize };
-                let p3: usize = program[p+3] as usize;
-                program[p3] = program[p1] * program[p2];
-                p += 4;
-            },
-            3 => { // Input
-                let addr: usize = program[p+1] as usize;
-                program[addr] = inputs.pop_front().unwrap();
-                p += 2;
-            },
-            4 => { // Output
-                let addr: usize = if inst%10 == 1 { p+1 } else { program[p+1] as usize };
-                outputs.push(program[addr]);
-                p += 2;
-            },
-            5 => { // jump-if-true
-                let p1: usize = if inst%10 == 1 { p+1 } else { program[p+1] as usize };
-                inst /= 10;
-                let p2: usize = if inst%10 == 1 { p+2 } else { program[p+2] as usize };
-
-                if program[p1] != 0 {
-                    p = program[p2] as usize;
-                } else {
-                    p += 3;
-                }
-            },
-            6 => { // jump-if-false
-                let p1: usize = if inst%10 == 1 { p+1 } else { program[p+1] as usize };
-                inst /= 10;
-                let p2: usize = if inst%10 == 1 { p+2 } else { program[p+2] as usize };
-
-                if program[p1] == 0 {
-                    p = program[p2] as usize;
-                } else {
-                    p += 3;
-                }
-            },
-            7 => { // less than
-                let p1: usize = if inst%10 == 1 { p+1 } else { program[p+1] as usize };
-                inst /= 10;
-                let p2: usize = if inst%10 == 1 { p+2 } else { program[p+2] as usize };
-                let p3: usize = program[p+3] as usize;
-
-                program[p3] = if program[p1] < program[p2] { 1 } else { 0 };
-
-                p += 4;
-            },
-            8 => { // equal to
-                let p1: usize = if inst%10 == 1 { p+1 } else { program[p+1] as usize };
-                inst /= 10;
-                let p2: usize = if inst%10 == 1 { p+2 } else { program[p+2] as usize };
-                let p3: usize = program[p+3] as usize;
-
-                program[p3] = if program[p1] == program[p2] { 1 } else { 0 };
-
-                p += 4;
-            }
-            _ => {}
-        }
-    }
-
-    outputs
 }
 
 #[cfg(test)]
