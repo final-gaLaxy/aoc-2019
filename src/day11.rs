b43c43f9b04c893535bfc3f7338cd093657d2b0b
final-gaLaxy <sebastian@genus.net.au>
2024-05-03 12:57:23 +1000

    day 11: add template

---
+++ src/day11.rs
@@ -0,0 +1,14 @@
+#[aoc(day11, part1)]
+pub fn solve_part1(input: &str) -> i32 {
+    0
+}
+
+#[aoc(day11, part2)]
+pub fn solve_part2(input: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
